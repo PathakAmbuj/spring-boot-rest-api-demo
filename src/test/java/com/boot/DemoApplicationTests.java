15d779d62885683577d56a4620a1d669dbb48f31
AmbujPathak <ambujnarayan.p@gmail.com>
2024-01-14 18:48:09 +0530

    Junit test run fixed

--- src/test/java/com/boot/DemoApplicationTests.java
+++ src/test/java/com/boot/DemoApplicationTests.java
@@ -1,15 +1,24 @@
 package com.boot;
 
+import com.boot.controller.EmployeeControllerTest;
 import com.boot.controller.ProductController;
+import com.boot.controller.ProductControllerTest;
+import com.boot.controller.TestProductController;
+import com.boot.service.ProductServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		ProductServiceTest.class,
+		EmployeeControllerTest.class,
+		ProductControllerTest.class,
+		TestProductController.class
+})
 class DemoApplicationTests {
-
-	@MockBean
-	public ProductController productController;
 
 	@Test
 	void contextLoads() {
